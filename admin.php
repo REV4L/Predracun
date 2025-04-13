e71e784425ca3c4b3eeb22f3044b437c5243488c
Nikola57ma <nikola.marinkovic@scv.si>
2025-04-13 15:25:04 +0200

    tzui

--- admin.php
+++ admin.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'a') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin panela</title>
-    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
 <div class="sidebar">
