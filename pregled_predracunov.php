5f40e3272692de1695a0c461bbfd322140907dde
Nikola57ma <nikola.marinkovic@scv.si>
2025-04-13 18:58:59 +0200

    FINALLYYYYYYYYYYYYYYYYYYY

--- pregled_predracunov.php
+++ pregled_predracunov.php
@@ -44,8 +44,7 @@ echo "Prijavljeni ste kot " . $_SESSION['ime'] . " " . $_SESSION['priimek'];
     </form>
 
     <?php
-    // Priprava osnovne poizvedbe in pogojev
-    $query = "SELECT p.st, p.dt, p.izdan, p.skupna_cena, p.koncna_cena, u.ime AS uporabnik_ime, u.priimek
+    $query = "SELECT p.id, p.datum, p.izdan, p.skupna_cena, p.koncna_cena, u.ime, u.priimek
               FROM predracun p
               JOIN uporabniki u ON p.uporabnik_id = u.id";
     
@@ -60,13 +59,13 @@ echo "Prijavljeni ste kot " . $_SESSION['ime'] . " " . $_SESSION['priimek'];
     }
 
     if (!empty($_POST['datum_od'])) {
-        $conditions[] = "p.dt >= ?";
+        $conditions[] = "p.datum >= ?";
         $params[] = $_POST['datum_od'];
         $types .= "s";
     }
 
     if (!empty($_POST['datum_do'])) {
-        $conditions[] = "p.dt <= ?";
+        $conditions[] = "p.datum <= ?";
         $params[] = $_POST['datum_do'];
         $types .= "s";
     }
@@ -75,47 +74,39 @@ echo "Prijavljeni ste kot " . $_SESSION['ime'] . " " . $_SESSION['priimek'];
         $query .= " WHERE " . implode(" AND ", $conditions);
     }
 
-    $query .= " ORDER BY p.dt DESC";
+    $query .= " ORDER BY p.datum DESC";
 
-    // Izpiši poizvedbo za diagnostiko
-    echo "<pre>$query</pre>";
+    $stmt = $link->prepare($query);
+    if (!empty($params)) {
+        $stmt->bind_param($types, ...$params);
+    }
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-    // Preverimo, če so parametri nastavljeni in jih povežemo
-    if ($stmt = $link->prepare($query)) {
-        if (!empty($params)) {
-            $stmt->bind_param($types, ...$params);
-        }
-        $stmt->execute();
-        $result = $stmt->get_result();
+    echo '<table border="1" style="border-collapse: collapse">';
+    echo '<tr>
+            <th>Št.</th>
+            <th>Datum</th>
+            <th>Izdan</th>
+            <th>Skupna cena</th>
+            <th>Končna cena</th>
+            <th>Uporabnik</th>
+          </tr>';
 
-        echo '<table border="1" style="border-collapse: collapse">';
-        echo '<tr>
-                <th>Št.</th>
-                <th>Datum</th>
-                <th>Izdan</th>
-                <th>Skupna cena</th>
-                <th>Končna cena</th>
-                <th>Uporabnik ime</th>
-              </tr>';
-
-        while ($row = mysqli_fetch_assoc($result)) {
-            $izdan = $row['izdan'] ? 'Da' : 'Ne';
-            echo "<tr>";
-            echo "<td>" . htmlspecialchars($row['st']) . "</td>";
-            echo "<td>" . htmlspecialchars($row['dt']) . "</td>";
-            echo "<td>" . $izdan . "</td>";
-            echo "<td>" . htmlspecialchars($row['skupna_cena']) . " €</td>";
-            echo "<td>" . htmlspecialchars($row['koncna_cena']) . " €</td>";
-            echo "<td>" . htmlspecialchars($row['uporabnik_ime']) . " " . htmlspecialchars($row['priimek']) . "</td>";
-            echo "</tr>";
-        }
-
-        echo '</table>';
-        $stmt->close();
-    } else {
-        echo "Napaka pri pripravi poizvedbe. Prosim preverite SQL poizvedbo.";
+    while ($row = mysqli_fetch_assoc($result)) {
+        $izdan = $row['izdan'] ? 'Da' : 'Ne';
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($row['id']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['datum']) . "</td>";
+        echo "<td>" . $izdan . "</td>";
+        echo "<td>" . htmlspecialchars($row['skupna_cena']) . " €</td>";
+        echo "<td>" . htmlspecialchars($row['koncna_cena']) . " €</td>";
+        echo "<td>" . htmlspecialchars($row['ime']) . " " . htmlspecialchars($row['priimek']) . "</td>";
+        echo "</tr>";
     }
 
+    echo '</table>';
+    $stmt->close();
     mysqli_close($link);
     ?>
 
