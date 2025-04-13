1ae9d28a1032d2f034d180c348788fc051ff1a33
Nikola57ma <nikola.marinkovic@scv.si>
2025-04-13 20:02:23 +0200

    jebem ti podčrtaj

--- blagajna.php
+++ blagajna.php
@@ -16,9 +16,9 @@ echo "<div class='pozdrav'>Prijavljeni ste kot " . $_SESSION['ime'] . " " . $_SE
 echo "<a href='odjava.php'>Odjava</a>";
 if ($isAdmin) {
     echo " | <a href='admin.php'>Nazaj na admin panel</a>";
-    echo " | <a href='pregled_predracunov_uporabnik.php'>Moji predračuni</a>" 
+    echo " | <a href='pregled_predracunov_uporabnik.php'>Moji predračuni</a>";
 }else{
-    echo " | <a href='pregled_predracunov_uporabnik.php'>Moji predračuni</a>" 
+    echo " | <a href='pregled_predracunov_uporabnik.php'>Moji predračuni</a>";
 }
 echo "</div>";
 ?>
